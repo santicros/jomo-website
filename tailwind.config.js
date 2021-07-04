11a26515c39083ad2ed27edbe2805b876a6b7120
Santi <scros97@gmail.com>
2021-07-04 18:10:57 +0200

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,15 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         extended: ['Monument Extended', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        black: {
+          DEFAULT: '#0C0C0D',
+        },
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require('@tailwindcss/typography')],
 };
